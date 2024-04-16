a795ed13cab959bf10ce891744027c316c030e16
Rayhan Adhisworo Jardine H <rayhan.jardine@gmail.com>
2024-04-16 21:13:05 +0700

    Create Notification service struct skeleton.

---
+++ src/service/notification.rs
@@ -0,0 +1,16 @@
+use std::thread;
+
+use rocket::http::Status;
+use rocket::log;
+use rocket::serde::json::to_string;
+use rocket::tokio;
+
+use bambangshop_receiver::{compose_error_response, Result, APP_CONFIG, REQWEST_CLIENT};
+use crate::model::notification::Notification;
+use crate::model::subscriber::SubscriberRequest;
+use crate::repository::notification::NotificationRepository;
+
+pub struct NotificationService;
+
+impl NotificationService {
+}
